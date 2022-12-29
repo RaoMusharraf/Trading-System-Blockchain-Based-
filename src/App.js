dfe5967396f11da7990e4de197798e176d98aafa
RaoMusharraf <mmusharraf829@gmail.com>
2022-12-29 22:38:12 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -15,20 +15,139 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [walletAddress, setWallet] = useState("");
+  const [SignIns, setSignIn] = useState("");
+  const [SignOut, setSignOut] = useState("");
+  const [address, setAddress] = useState("");
+  const [name, setName] = useState("");
+  const [signer, setSigner] = useState(false);
+  const [email, setEmail] = useState("");
+  const [show, setShow] = useState(false);
   // const [Tokentime, setTokentime] = useState({});
   const [status, setStatus] = useState("");
   // const [auctionDetails, setAuctionDetails] = useState([]);
   // const [timer, setmushi] = useState("");
-  // const web3 = new Web3(window.ethereum);
-  // const auctionContract = process.env.REACT_APP_CONTRACT;
-  // const contractAuctionABI = require('../abi/abi_tender.json');
-  // window.contract = new web3.eth.Contract(contractAuctionABI, auctionContract);
-
+  const web3 = new Web3(window.ethereum);
+  const auctionContract = process.env.REACT_APP_CONTRACT;
+  const contractAuctionABI = require('./abi/abi_tender.json');
+  window.contract = new web3.eth.Contract(contractAuctionABI, auctionContract);
+  function timeout(delay) {
+    return new Promise(res => setTimeout(res, delay));
+  }
   const connectWalletPressed = async () => {
     const walletResponse = await connectWallet();
     setStatus(walletResponse.status);
     setWallet(walletResponse.address);
   };
+  const handleClose = () => setShow(false);
+  const handleShow = async () => {
+    setShow(true);
+  };
+  const signUp = async () => {
+
+    console.log(name);
+    console.log(email);
+    console.log(address);
+    const transactionParameters = {
+      to: auctionContract, // Required except during contract publications.
+      from: address, // must match user's active address.
+      'data': window.contract.methods.SignUp(name, email, window.ethereum.selectedAddress).encodeABI()//make call to NFT smart contract
+    };
+    // console.log("wdsededed");
+    //sign the transaction via Metamask
+    const txHash = await window.ethereum
+      .request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
+    // setStatus("✅ Check out your transaction on Etherscan .");
+    for (let index = 0; index > -1; index++) {
+      var receipt = await web3.eth.getTransactionReceipt(txHash)
+      if (receipt != null) {
+        // setTransec(receipt);
+        await window.contract.methods.Signer(window.ethereum.selectedAddress).call().then(res => {
+          setSigner(res.Up);
+        });
+
+        break;
+      }
+      await timeout(1000);
+      console.log("Hello");
+    }
+
+  }
+
+  const mysignersetter = async () => {
+    await window.contract.methods.Signer(window.ethereum.selectedAddress).call().then(res => {
+      setSigner(res.Up);
+      setSignIn(res.InOut);
+    })
+  }
+
+  useEffect(() => {
+    mysignersetter()
+
+  }, [])
+  const signIn = async () => {
+    try {
+      // const all_single = await window.contract.methods.SignIn("0x2B8C026e5a69Af0A8e7D670f45f8E16c0362a6d3").call();
+      const transactionParameters = {
+        to: auctionContract, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        'data': window.contract.methods.SignIn(window.ethereum.selectedAddress).encodeABI()//make call to NFT smart contract
+      };
+      const txHash = await window.ethereum
+        .request({
+          method: 'eth_sendTransaction',
+          params: [transactionParameters],
+        });
+      // setStatus("✅ Check out your transaction on Etherscan .");
+      for (let index = 0; index > -1; index++) {
+        var receipt = await web3.eth.getTransactionReceipt(txHash)
+        if (receipt != null) {
+          // setTransec(receipt);
+          await window.contract.methods.Signer(window.ethereum.selectedAddress).call().then(res => {
+            setSignIn(res.InOut);
+          });
+
+          break;
+        }
+        await timeout(1000);
+        console.log("Hello");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const signOut = async () => {
+    try {
+      const transactionParameters = {
+        to: auctionContract, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        'data': window.contract.methods.SignOut(window.ethereum.selectedAddress).encodeABI()//make call to NFT smart contract
+      };
+      const txHash = await window.ethereum
+        .request({
+          method: 'eth_sendTransaction',
+          params: [transactionParameters],
+        });
+      // setStatus("✅ Check out your transaction on Etherscan .");
+      for (let index = 0; index > -1; index++) {
+        var receipt = await web3.eth.getTransactionReceipt(txHash)
+        if (receipt != null) {
+          // setTransec(receipt);
+          await window.contract.methods.Signer(window.ethereum.selectedAddress).call().then(res => {
+            setSignIn(res.InOut);
+          });
+
+          break;
+        }
+        await timeout(1000);
+        console.log("Hello");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
   const addWalletListener = async () => {
     if (window.ethereum) {
       window.ethereum.on("accountsChanged", (accounts) => {
@@ -57,11 +176,31 @@ function App() {
     const { address, status } = await getCurrentWalletConnected();
     setWallet(address);
     setStatus(status);
+    connectWalletPressed();
     addWalletListener();
   }, []);
   return (
-
     <>
+      <Modal className='signupmodel' show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Requests</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='mysignup-wrap'>
+            <label>Name</label>
+            <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Name" />
+            <label>Email</label>
+            <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+            <label>Address</label>
+            <input onChange={(e) => setAddress(e.target.value)} placeholder="Address" />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={() => signUp()}>
+            Sign UP
+          </button>
+        </Modal.Footer>
+      </Modal>
       <div class="topnav">
         <div>
           <h1 id="title" style={{ textAlign: 'center' }}>
@@ -74,19 +213,23 @@ function App() {
           <NavLink to="/">All Tenders</NavLink>
           <NavLink to="/bundle_auction">Vender Requests</NavLink>
           <NavLink to="/Vender_request">Create Vender</NavLink>
-          <button id="walletButton" onClick={connectWalletPressed}>
+          <button id="walletButton">
+
             {walletAddress.length > 0 ? (
-              "Connected: " +
-              String(walletAddress).substring(0, 6) +
+              String(walletAddress).substring(0, 4) +
               "..." +
               String(walletAddress).substring(38)
             ) : (
               <span>Connect Wallet</span>
             )}
           </button>
-
+          {
+            SignIns ? <>
+              <button onClick={signOut}>Sign out</button>
+            </> : signer ? <button onClick={signIn}>Sign in</button> :
+              <button onClick={handleShow}>Sign Up</button>
+          }
         </div>
-
       </div>
       <Routes>
         <Route path='/' element={<AllItems />}></Route>
